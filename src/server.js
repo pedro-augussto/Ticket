0b098671efbbf118429657d0b35a66bfe9d977a4
Pedro Augusto <pa.severosilva@gmail.com>
2024-10-23 10:29:33 -0300

    Criando o banco de dados

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { routeHandler } from "./middlewares/routeHandler.js";
 
 async function listener(request, response) {
   await jsonHandler(request, response);
-  routeHandler(request,response)
+  routeHandler(request, response);
 }
 
 http.createServer(listener).listen(3333);
