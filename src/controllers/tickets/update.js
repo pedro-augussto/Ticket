d4fef26dfc9d5a7c02fb84f3ad7e00c27f4a62d0
Pedro Augusto <pa.severosilva@gmail.com>
2024-10-23 17:27:47 -0300

    Obtendo dados do ticket

--- src/controllers/tickets/update.js
+++ src/controllers/tickets/update.js
@@ -1,3 +1,7 @@
-export function update({ request, response , database }){
-   return response.end("Ok")
+export function update({ request, response, database }) {
+  const { id } = request.params;
+
+  const { equipment, description } = request.body
+
+  return response.end();
 }
