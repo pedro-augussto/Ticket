6fa06a633457319138b8f4e14b579f220fbe598b
Pedro Augusto <pa.severosilva@gmail.com>
2024-10-23 17:50:32 -0300

    Atualizando o ticket

--- src/controllers/tickets/update.js
+++ src/controllers/tickets/update.js
@@ -1,7 +1,12 @@
 export function update({ request, response, database }) {
   const { id } = request.params;
+  const { equipment, description } = request.body;
 
-  const { equipment, description } = request.body
+  database.update("tickets", id, {
+    equipment,
+    description,
+    updated_at: new Date(),
+  });
 
   return response.end();
 }
--- src/database/database.js
+++ src/database/database.js
@@ -37,11 +37,24 @@ export class Database {
         return Object.entries(filters).some(([key, value]) => {
           return row[key].toLowerCase().includes(value.toLowerCase());
         });
-        
+
         return row;
       });
     }
 
     return data;
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = {
+        ...this.#database[table][rowIndex],
+        ...data,
+      };
+
+      this.#persist();
+    }
   }
 }
